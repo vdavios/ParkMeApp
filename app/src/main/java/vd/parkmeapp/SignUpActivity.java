a001ea417ba0a97c4a9a4df70bf26aff0453c503
vdavios <vas.davios@gmail.com>
2017-06-26 19:39:56 +0100

    Sign Up page

--- app/src/main/java/vd/parkmeapp/SignUpActivity.java
+++ app/src/main/java/vd/parkmeapp/SignUpActivity.java
@@ -3,11 +3,11 @@ package vd.parkmeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.singup);
     }
 }
--- app/src/main/java/vd/parkmeapp/testing.java
+++
@@ -1,9 +0,0 @@
-package vd.parkmeapp;
-
-
-public class testing {
-    private int x;
-    public testing(){
-        x =5 ;
-    }
-}
