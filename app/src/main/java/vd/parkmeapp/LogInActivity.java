f990ea27412e922fc211223d833f351007e5eda3
vdavios <vas.davios@gmail.com>
2017-07-05 19:22:42 +0100

    Google Maps added

---
+++ app/src/main/java/vd/parkmeapp/LogInActivity.java
@@ -0,0 +1,41 @@
+package vd.parkmeapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LogInActivity extends AppCompatActivity {
+    Button newUserButton;
+    Button logInButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        logInButton = (Button) findViewById(R.id.LogInButton);
+        logInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                parkMeAppMainAct(v);
+            }
+        });
+
+        newUserButton = (Button) findViewById(R.id.SingUpButton);
+        newUserButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Toast.makeText(getApplicationContext(),R.string.email, Toast.LENGTH_LONG)
+                            .show();
+                }
+        });
+
+    }
+
+    private void parkMeAppMainAct(View v) {
+        Intent intent = new Intent(this, ParkMeAppMainActivity.class);
+        startActivity(intent);
+    }
+}
